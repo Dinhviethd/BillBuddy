e67e5c9f118caeaf474912fba8dbb6a69b793fc9
FozzyWork <khanh05work@gmail.com>
2026-05-07 00:14:01 +0700

    feat: added statistics UI

--- app/src/main/java/com/example/billbuddy/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/billbuddy/ui/screens/home/HomeScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.example.billbuddy.ui.viewmodel.AuthViewModel
 
